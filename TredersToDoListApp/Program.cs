324433b6da657742cda13d3b2e63759fd329c2dc
Mastermind <Mastermind@DESKTOP-NNTQBM8>
2024-12-02 22:32:02 +0100

    created project structure

--- TredersToDoListApp/Program.cs
+++ TredersToDoListApp/Program.cs
@@ -1,14 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
